625909a82b4fd1dc914c8809c48e87a63688664a
DmitryGK <gulyakevichdmitry@gmail.com>
2022-12-19 16:06:41 +0300

    tests_01

--- src/01/01.tsx
+++ src/01/01.tsx
@@ -2,16 +2,22 @@
 
 export const value01_01 = '01_01'
 
-export const sentence = 'Hello my friends'
 
-export function splitIntoWords(sentence:string) {
-    return [ 'Hello', 'my', 'friends']
+
+export function splitIntoWords(sentence: string) {
+    const words = sentence.toLowerCase().split(' ')
+
+    return words.filter(w => w !== '' && w !== '-')
+        .map(w => w
+            .replace('!', '')
+            .replace('-', '')
+        )
 }
 
-export function sum(a:number, b: number) {
+export function sum(a: number, b: number) {
     return a + b
 }
 
-export function mult(a:number, b: number) {
+export function mult(a: number, b: number) {
     return a * b
 }
